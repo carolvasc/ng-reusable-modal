55dc3527e42b4b7925602473a6f283f3c2ce09f7
Carol Vasconcelos <cvsouza04@gmail.com>
2020-04-24 12:34:00 -0300

    Implementação dos serviços de mock no serviço de modal

--- src/app/services/modal-actions.service.ts
+++ src/app/services/modal-actions.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@angular/core';
+import { MockServ1Service } from './mock-serv-1.service';
+import { MockServ2Service } from './mock-serv-2.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ModalActionsService {
 
-  constructor() { }
+  constructor(
+    private serv1: MockServ1Service,
+    private serv2: MockServ2Service
+  ) { }
 
   modalAction(modalData: any) {
     switch (modalData.name) {
@@ -22,11 +27,12 @@ export class ModalActionsService {
     }
   }
 
+
   private logout(modalData: any) {
-    console.log("I came from a logout modal");
+    this.serv1.alertLogout(modalData);
   }
 
   private deleteProduct(modalData: any) {
-    console.log("I came from a product deletion modal");
+    this.serv2.alertDelete(modalData)
   }
 }
