d7cb95a25087743e752e00d379239efe9a399335
Carol Vasconcelos <cvsouza04@gmail.com>
2020-04-24 12:29:43 -0300

    Criação do serviço do modal

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { ModalActionsService } from '../../services/modal-actions.service';
 
 @Component({
   selector: 'app-modal',
@@ -11,6 +12,7 @@ export class ModalComponent implements OnInit {
   constructor(
     public dialogRef: MatDialogRef<ModalComponent>,
     @Inject(MAT_DIALOG_DATA) private modalData: any,
+    private modalService: ModalActionsService
   ) { 
     console.log(this.modalData);
   }
@@ -18,6 +20,7 @@ export class ModalComponent implements OnInit {
   ngOnInit() { }
 
   actionFunction() {
+    this.modalService.modalAction(this.modalData);
     this.closeModal();
   }
 
